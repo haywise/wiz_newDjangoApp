018d2a1165b6ac4c4b27fbd59748730fc2bfe67c
haywise <152945861+haywise@users.noreply.github.com>
2024-02-15 19:01:55 -0600

    Update ec2.tf

--- kubernetes/ec2.tf
+++ kubernetes/ec2.tf
@@ -1,115 +1,58 @@
-# configured aws provider with proper credentials
+# Configure AWS provider with proper credentials
 provider "aws" {
-  region    = "us-east-1"
-  profile   = "Abdulhakeem"
+  region  = "us-east-1"
+  profile = "Abdulhakeem"
 }
 
+# Use the existing VPC if one does not exist
+resource "aws_vpc" "myAppp-vpc" {
 
-# create default vpc if one does not exit
-resource "aws_vpc" "myapp-vpc" {
-
-  tags    = {
-    Name  = "myapp-vpc"
+  tags = {
+    Name = "my-vpc"
   }
 }
 
+# Use the first public subnet from the module as the subnet for the EC2 instance
+data "aws_subnet" "public_subnet" {
+  count = length(module.myAppp-vpc.public_subnets)
 
-# use data source to get all avalablility zones in region
-data "aws_availability_zones" "available_zones" {}
-
-
-# create default subnet if one does not exit
-
-data "aws_subnet" "public_subnets" {
-  vpc_id = data.aws_vpc.myapp-vpc.id
-
-  tags   = {
-    Name = "public subnet"
-  }
+  vpc_id     = module.myAppp-vpc.vpc_id
+  cidr_block = module.myAppp-vpc.public_subnets[count.index]
 }
 
-
-# create security group for the ec2 instance
+# Create security group for the EC2 instance
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
-  description = "allow access on ports 8080 and 22"
-  vpc_id      = aws_default_vpc.default_vpc.id
+  description = "allow access on ports 8080, 22, 80, and 443"
+  vpc_id      = module.myAppp-vpc.vpc_id
 
-  # allow access on port 8080
-  ingress {
-    description      = "http proxy access"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  # allow access on port 22
-  ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  } 
-  ingress {
-    description      = "http access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  
-   ingress {
-    description      = "https access"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags   = {
-    Name = "jenkins server security group"
-  }
+  # ... (rest of your security group configuration)
 }
 
-
-# use data source to get a registered amazon linux 2 ami
+# Use data source to get a registered Amazon Linux 2 AMI
 data "aws_ami" "ubuntu" {
+  most_recent = true
 
-    most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
 
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-
-    owners = ["099720109477"]
+  owners = ["099720109477"]
 }
 
-# launch the ec2 instance
+# Launch the EC2 instance
 resource "aws_instance" "ec2_instance" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.small"
-  subnet_id              = module.myapp-vpc.public_subnets[0]
+  subnet_id              = data.aws_subnet.public_subnet[0].id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   key_name               = "kim"
-  # user_data = "${file("install_jenkins.sh")}"
-
   tags = {
     Name = "mongodb_server"
   }
@@ -124,7 +67,7 @@ resource "aws_s3_bucket" "resource_name" {
   }
 }
 
-# print the url of the jenkins server
+# Print the URL of the MongoDB server
 output "website_url" {
-  value     = join("", ["http://", aws_instance.ec2_instance.public_ip])
+  value = join("", ["http://", aws_instance.ec2_instance.public_ip])
 }
